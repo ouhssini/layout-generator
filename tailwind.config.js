fe1e8fd0b230736078935670ddd24b2b2a9de059
Abdessamad Bouih <151373460+abdessamad10bouih@users.noreply.github.com>
2024-11-02 11:35:25 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: "jit",
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}", // Make sure your paths are correct
